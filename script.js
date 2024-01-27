8c3cc47117a9ccd26320689c3c4d8fe58fa0996c
F-zeinali <fatemeh.zeinali7026@gmail.com>
2024-01-27 23:01:50 +0330

    set localstorage

--- script.js
+++ script.js
@@ -4,36 +4,53 @@ let listGroupItem = $.querySelectorAll(".list-group-item");
 let addBtn = $.querySelector(".btn");
 let inputBox = $.querySelector(".form-control");
 let trash = $.querySelectorAll(".delete");
-
+let toDoArray = [];
 function addNewItem() {
-  let newLi = $.createElement("li");
-  newLi.className = "list-group-item";
+  let newObj = {
+    id: toDoArray.length + 1,
+    title: inputBox.value,
+    complete: false,
+  };
 
-  let newIconTrash = $.createElement("i");
-  newIconTrash.className = "fa fa-trash-o delete";
-
-  let newIconContainer = $.createElement("div");
-  newIconContainer.className = "icon-container";
-
-  newIconTrash.addEventListener("click", function (event) {
-    event.target.parentElement.parentElement.remove();
-  });
-  let newIconCheck = $.createElement("i");
-  newIconCheck.className = "fa fa-solid-o fa-check check";
-
-  newIconCheck.addEventListener("click", function () {
-    newSpan.classList.toggle("active");
-  });
-  newIconContainer.append(newIconTrash, newIconCheck);
-
-  let newSpan = $.createElement("span");
-  newSpan.innerHTML = inputBox.value;
-
-  newLi.append(newSpan, newIconContainer);
-
-  listGroup.append(newLi);
-
+  toDoArray.push(newObj);
+  setLocalStorage(toDoArray);
+  toDoGenerator(toDoArray);
   inputBox.value = "";
+  console.log(toDoArray);
+}
+
+function setLocalStorage(a) {
+  localStorage.setItem("todo", JSON.stringify(a));
+}
+function toDoGenerator(a) {
+  a.forEach(() => {
+    let newLi = $.createElement("li");
+    newLi.className = "list-group-item";
+
+    let newIconTrash = $.createElement("i");
+    newIconTrash.className = "fa fa-trash-o delete";
+
+    let newIconContainer = $.createElement("div");
+    newIconContainer.className = "icon-container";
+
+    newIconTrash.addEventListener("click", function (event) {
+      event.target.parentElement.parentElement.remove();
+    });
+    let newIconCheck = $.createElement("i");
+    newIconCheck.className = "fa fa-solid-o fa-check check";
+
+    newIconCheck.addEventListener("click", function () {
+      newSpan.classList.toggle("active");
+    });
+    newIconContainer.append(newIconTrash, newIconCheck);
+
+    let newSpan = $.createElement("span");
+    newSpan.innerHTML = inputBox.value;
+
+    newLi.append(newSpan, newIconContainer);
+
+    listGroup.append(newLi);
+  });
 }
 
 addBtn.addEventListener("click", function (event) {
